596ee30dd41dbbf13dfbded1e90e2c67d5eaa1bb
Marcin Majewski <martinpl@gmail.com>
2022-05-03 19:11:29 +0200

    After stage 2 of hyperskill django track (news parsed from news.json /news/1/, main /news/ page added)

--- HyperNews Portal/task/news/urls.py
+++ HyperNews Portal/task/news/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('news/', views.news, name='news'),
+    path('news/<int:link_id>/', views.news, name='news_detail'),
+    path('news/', views.news, name='all news'),
 ]
--- HyperNews Portal/task/news/views.py
+++ HyperNews Portal/task/news/views.py
@@ -1,7 +1,22 @@
+import os.path
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+import json
 
 
 # Create your views here.
 def home(request):
     return HttpResponse("Coming soon")
+
+
+def news(request, link_id=None):
+    full_json_path = os.path.join(settings.BASE_DIR, 'hypernews\\', settings.NEWS_JSON_PATH)
+    with open(full_json_path, 'r') as f:
+        news_json = json.load(f)
+        for item in news_json:
+            if item['link'] == link_id:
+                return render(request, 'news/news_detail.html', item)
+        # news/
+        return HttpResponse("News not found")
